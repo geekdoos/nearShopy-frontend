33e31bf18df486d7dd904281abd83eb54ef0de13
KHACHIAI Oussama <oussama.khachiai@gmail.com>
2019-01-19 12:43:26 +0100

    SigneUp forme for users

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -6,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  public form = {
+    email: null,
+    name: null,
+    password: null,
+    password_confirmation: null
+  };
+  public error = [];
+  constructor(private http: HttpClient) { }
+  onSubmit() {
+    return this.http.post('http://127.0.0.1:8000/api/signup', this.form).subscribe(
+      data => console.log(data),
+      error => this.handleError(error)
+    );
+  }
+  handleResponse(data) {
+  }
 
-  constructor() { }
-
+  handleError(error) {
+    this.error = error.error.errors;
+  }
   ngOnInit() {
   }
 
